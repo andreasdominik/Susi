caafd95a302515c01d60b89dabc15321cbf2f63a
andreas.dominik <andreas.dominik@mni.thm.de>
2020-01-07 16:34:38 +0100

    restart of daemons impl.

--- src/Service/susi.sh
+++ src/Service/susi.sh
@@ -2,7 +2,7 @@
 #
 # Basic service for NoSnips replacement service.
 # Usage:
-#     nosnips.sh path/to/nosnips.toml
+#     susi.sh path/to/susi.toml
 #
 # https://github.com/dbohdan/remarshal is used for reading toml
 # jq is used for parsing JSON
@@ -12,11 +12,11 @@
 # and make a JSON config in work dir:
 #
 if [[ $# -lt 1 ]] ; then
-  CONFIG="/etc/nosnips.toml"
+  CONFIG="/etc/susi.toml"
 else
   CONFIG=$1
 fi
-cd $WORK_DIR
+cd /tmp
 cat $CONFIG | toml2json > susi.json
 
 TOML="$(cat susi.json)"
@@ -26,14 +26,16 @@ export BASE_DIR
 
 # load tool funs:
 #
-source $BASE_DIR/Tools/funs.sh
+source $BASE_DIR/src/Tools/funs.sh
 
 IS_SATELLITE="$(extractJSON .local.satellite $TOML)"
 if [[ $IS_SATELLITE == true ]] ; then
   DAEMONS="hotword record say"
 else
-  DAEMONS="hotword record say stt nlu tts session"
+  DAEMONS="hotword record say stt nlu duckling tts session"
 fi
+
+MONITOR_PIDS=""
 
 function startDaemon() {
 
@@ -43,10 +45,40 @@ function startDaemon() {
   if [[ $_FLAG == true ]] ; then
     echo "starting $_DAEMON daemon"
     _EXEC="$(extractJSONdir .$_DAEMON.daemon $TOML)"
-    $BASE_DIR/$_EXEC $CONFIG &
+    $_EXEC $CONFIG &
+
+    # make list to monitor:
+    #
+    _PID=$!
+    MONITOR_PIDS="$MONITOR_PIDS $_PID $_DAEMON"
   fi
 }
 
 for DAEMON in $DAEMONS ; do
   startDaemon $DAEMON
+done
+
+
+# monitor all daemons and restart:
+#
+while true ; do
+  sleep 10
+  set -- $MONITOR_PIDS
+  MONITOR_PIDS=""
+
+  while [[ $# -gt 1 ]] ; do
+    __PID=$1
+    __DAEMON=$2
+    shift 2
+
+    ps -p $__PID
+    __IS_OK=$?
+
+    if [[ $__IS_OK -gt 0 ]] ; then
+      startDaemon $__DAEMON
+    else
+      MONITOR_PIDS="$MONITOR_PIDS $__PID $__DAEMON"
+    fi
+    sleep 1
+  done
 done
