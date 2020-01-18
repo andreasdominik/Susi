80fb3644c7ad96b919f9c199c40c5a6263447792
andreas.dominik <andreas.dominik@mni.thm.de>
2020-01-18 11:24:01 +0100

    debug tool added

---
+++ bin/runDebug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xv
+#
+# open all deamons in separate terminal tabs
+#
+#
+
+gnome-terminal --tab -t "Session Manager" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Session/session.daemon
+gnome-terminal --tab -t "Hotword" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Hotword/hotword.daemon
+gnome-terminal --tab -t "NLU" --working-directory=/tmp -- $SUSI_INSTALLATION/src/NLU/nlu.daemon.jl
+gnome-terminal --tab -t "Play" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Play/play.daemon
+gnome-terminal --tab -t "Record" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Record/record.daemon
+gnome-terminal --tab -t "STT" --working-directory=/tmp -- $SUSI_INSTALLATION/src/SpeechToText/stt.daemon
+gnome-terminal --tab -t "TTS" --working-directory=/tmp -- $SUSI_INSTALLATION/src/TextToSpeech/tts.daemon
+# gnome-terminal --tab -t "Skills" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Skills/skills.daemon
