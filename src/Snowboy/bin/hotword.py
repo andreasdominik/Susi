6b4020485b0b49809e925a69e036e1891bac63bd
andreas.dominik <andreas.dominik@mni.thm.de>
2020-01-19 10:49:51 +0100

    hotword can be triggered off now.

--- src/Snowboy/bin/hotword.py
+++ src/Snowboy/bin/hotword.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 #
+# usage: hotword.py model senistivity trigger_file
 import snowboydecoder
 import sys
 import signal
+import os.path
+from os import path
 
 interrupted = False
 
@@ -16,13 +19,14 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     print("Error: need to specify model name and sensitivity")
-    print("Usage: python hotword.py your.path/your.model 0.5")
+    print("Usage: python hotword.py your.path/your.model 0.5 trigger.off")
     sys.exit(-1)
 
 model = sys.argv[1]
 sensi = sys.argv[2]
+offTrigger = sys.argv[3]
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
@@ -33,13 +37,15 @@ detector = snowboydecoder.HotwordDetector(model, sensitivity=sensi)
 # reason = detector.start(detected_callback=snowboydecoder.play_audio_file,
 #                interrupt_check=interrupt_callback,
 #                sleep_time=0.03)
-reason = detector.start(detected_callback=snowboydecoder.print_detected,
+while True:
+    reason = detector.start(detected_callback=snowboydecoder.print_detected,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
-detector.terminate()
+    detector.terminate()
 
-if reason == "hotword":
-    sys.exit(0)
-else:
-    sys.exit(1)
+    if not path.exists(offTrigger)
+        if reason == "hotword":
+            sys.exit(0)
+        else:
+            sys.exit(1)
