7ae7cde78c8ab87141886bfd1fae3e9e9c1193c8
andreas.dominik <andreas.dominik@mni.thm.de>
2020-01-30 17:36:21 +0100

    duckling integrated

--- bin/runDebug.sh
+++ bin/runDebug.sh
@@ -7,6 +7,7 @@
 gnome-terminal --tab -t "Session Manager" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Session/session.daemon
 gnome-terminal --tab -t "Hotword" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Hotword/hotword.daemon
 gnome-terminal --tab -t "NLU" --working-directory=/tmp -- $SUSI_INSTALLATION/src/NLU/nlu.daemon.jl
+gnome-terminal --tab -t "Duckling" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Duckling/duckling.daemon
 gnome-terminal --tab -t "Play" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Play/play.daemon
 gnome-terminal --tab -t "Record" --working-directory=/tmp -- $SUSI_INSTALLATION/src/Record/record.daemon
 gnome-terminal --tab -t "STT" --working-directory=/tmp -- $SUSI_INSTALLATION/src/SpeechToText/stt.daemon
