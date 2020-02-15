b216d14ab16ce3d4ef43428d03032cd1331887d8
andreas.dominik <andreas.dominik@mni.thm.de>
2020-02-15 16:38:24 +0100

    deepspeech stt added

---
+++ src/MozillaDeepSpeech/stt-deepspeech.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# use Mozilla DeesSpeech for SST
+#   Input: $1 : file with base64-encoded audio
+#   Output: $2 file with transscript only
+
+STT_INPUT=$1
+LANGUAGE=$2
+STT_OUTPUT=$3
+
+CONFIG="/etc/susi.toml"
+source $SUSI_INSTALLATION/bin/toml2env $CONFIG
+
+# load tool funs:
+#
+source $SUSI_INSTALLATION/src/Tools/funs.sh
+
+
+set -xv
+# only english:
+#
+if [[ $LANGUAGE != "en" ]] ; then
+  echo "" > $STT_OUTPUT
+  exit 1
+fi
+
+if [[ ! -s $STT_INPUT ]] ; then
+  echo "" > $STT_OUTPUT
+  exit 1
+fi
+
+# make wav from b64:
+#
+AUDIO_RAW="audio"
+AUDIO_WAV=${AUDIO_RAW}.wav
+base64 --decode $STT_INPUT > $AUDIO_RAW
+
+# use ffmpg for format detection rather then soxi:
+#
+MEDIA_TYPE="$(ffprobe -show_format $AUDIO_RAW 2>/dev/null | grep -Po '(?<=format_name=)[0-9a-zA-Z]+$')"
+AUDIO_NAME="${AUDIO_RAW}.${MEDIA_TYPE}"
+sox $AUDIO $AUDIO_WAV
+
+
+DEEP_SPEECH="$deep_speech_binary"
+INSTALLATION_DIR="$(relDir $deep_speech_installation)"
+MODEL_DIR="$deep_speech_model_dir"
+MODEL="$deep_speech_model"
+LANGUAGE_MODEL="$deep_speech_language_model"
+TRIE="$deep_speech_trie"
+
+# virtualenv -p python3 $HOME/tmp/deepspeech-venv/
+cd $INSTALLATION_DIR
+source ./deepspeech-venv/bin/activate
+
+$DEEP_SPEECH --model $MODEL_DIR/$MODEL \
+       --lm $MODEL_DIR/$LANGUAGE_MODEL \
+       --trie $MODEL_DIR/$TRIE \
+       --audio $local_work_directory/$AUDIO_WAV > $local_work_directory/$STT_OUTPUT
+
+echo "transscript is:"
+cat $local_work_directory/$STT_OUTPUT
