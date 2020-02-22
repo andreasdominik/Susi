98dd92e29d0f9bbea8d6d20ee8616d9337af3437
andreas.dominik <andreas.dominik@mni.thm.de>
2020-02-22 09:50:59 +0100

    read config file in separate file

---
+++ src/Tools/init_susi.sh
@@ -0,0 +1,18 @@
+# read the toml file and init env for Susi
+#
+CONFIG="/etc/susi.toml"
+source $SUSI_INSTALLATION/bin/toml2env $CONFIG
+
+# get 2-digit language code:
+#
+LANGUAGE_CODE="${assistant_language/_/-}"
+LANGUAGE=${assistant_language:0:2}
+
+# load tool funs:
+#
+source $SUSI_INSTALLATION/src/Tools/funs.sh
+source $SUSI_INSTALLATION/src/Tools/topics.sh
+
+# counter for recveived and subm. MQTT message files:
+#
+MQTT_COUNTER=0
