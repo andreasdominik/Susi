545e708e2f7a81b8a1ff47b363515c811a4420ec
andreas.dominik <andreas.dominik@mni.thm.de>
2020-02-29 11:58:53 +0100

    duckling service replaced by local Rustling

--- src/NLU/Modules/NLU/duckling.jl
+++ src/NLU/Modules/NLU/duckling.jl
@@ -1,9 +1,16 @@
+function askDuckling(dimension, text)
+    return askRustling(dimension, text)
+end
+
+
+
+
 """
     askDuckling(dimension, text)
 
-Call duckling web-service to read the slot content.
+Call duckling to read the slot content.
 """
-function askDuckling(dimension, text)
+function askDucklingService(dimension, text)
 
     cmd = `curl -v -XPOST http://$DUCKLING_HOST:$DUCKLING_PORT/parse --data lang=$LANG\&text=$text -o duckling.out`
 
@@ -48,5 +55,113 @@ function askDuckling(dimension, text)
         slotvalue = ""
     end
 
+    return slotvalue
+end
+
+
+
+
+"""
+    askRustling(dimension, text)
+
+Call duckling to read the slot content.
+"""
+function askRustling(dimension, text)
+
+    IN_FILE = "rustling.in"
+    OUT_FILE = "rustling.out"
+
+    try
+        rm(OUT_FILE)
+    catch
+    end
+    # create input file for rustling service:
+    #
+    rawSlot = Dict("sentence" => text)
+    open(IN_FILE, "w") do f
+        JSON.print(f, rawSlot)
+    end
+
+    # wait until result is there:
+    #
+    time = 0
+    while !isfile(OUT_FILE) && (time < 5)
+        sleep(0.01)
+        time += 0.01
+    end
+
+    # read rustling result:
+    #
+    slotvalue = ""
+    try
+        rustlingOut = strip(read(OUT_FILE, String))
+        rustlingOut = replace(rustlingOut, "\r" => "")
+        rustlingOut = replace(rustlingOut, "\n" => "")
+
+        println("from file: $rustlingOut")
+        # return time w/o timezone:
+        #
+        if dimension == "Time"
+            regEx = r"^Datetime\(DatetimeOutput { moment: (\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d),"
+            #slotvalue = replace(value, r"[+-]\d\d:\d\d$" => "")  # rm timezone
+            m = match(regEx, rustlingOut)
+            if m != nothing
+                slotvalue = m[1]
+            end
+
+        # return numbers as numbers (not string):
+        #
+        elseif dimension in ["Number", "Ordinal"]
+            regEx = r"^(?:Integer\(IntegerOutput|Ordinal\(OrdinalOutput|Float\(FloatOutput)\(([0-9,\.]+)\)\)"
+            m = match(regEx, rustlingOut)
+            println(regEx)
+            println(rustlingOut)
+            if m != nothing
+                slotvalue = m[1]
+            end
+
+        # return currency as string with unit like "5 EUR":
+        #
+        elseif dimension == "Currency"
+            regEx = r"^AmountOfMoney\(AmountOfMoneyOutput \{ value: ([0-9,\.]+),.*unit.*\(\"(.+)\"\)"
+            m = match(regEx, rustlingOut)
+            if m != nothing
+                slotvalue = "$(m[1]) $(m[2])"
+            else
+                regEx = r"^(?:Integer\(IntegerOutput|Ordinal\(OrdinalOutput|Float\(FloatOutput)\(([0-9,\.]+)\)\)"
+                m = match(regEx, rustlingOut)
+                if m != nothing
+                   slotvalue = m[1]
+                end
+            end
+
+
+        # return duration always as number of secs:
+        #
+        elseif dimension == "Duration"
+            regEx = r"^Duration\(DurationOutput \{ period: Period\(\{([0-9]): ([0-9,\.]+)\}\)"
+            m = match(regEx, rustlingOut)
+
+            # make unit:
+            #
+            if m != nothing
+                unitNum = tryparse(Int, m[1])
+                val = tryparse(Int, m[2])
+
+                if unitNum != nothing && val != nothing && 0 ≤ unitNum ≤ 7
+                    units = ["year", "unknown","month", "week", "day", "hour", "minute", "second"]
+                    unit = units[unitNum+1]
+                    slotvalue = "$val $unit"
+                else
+                    slotvalue = "0"
+                end
+            end
+        else
+            m = nothing
+        end
+
+    catch
+        slotvalue = ""
+    end
     return slotvalue
 end
